cb3531fdd214e86065ff6e2e16aea7c8e5f985a0
gaba <harpuneet.gaba@csuglobal.edu>
2024-06-12 21:56:20 -0700

    adds

---
+++ division_multiplication.py
@@ -0,0 +1,6 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+multiplication = float(num1) * float(num2) 
+Division = float(num1) / float(num2)
+print("Multiplication result: " + str(multiplication))
+print ("Division result: " + str(Division))
